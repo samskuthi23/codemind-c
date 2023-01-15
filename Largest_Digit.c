d300870c31676699171a7f4f626baf4c65c13131
samskuthi23 <119730613+samskuthi23@users.noreply.github.com>
2023-01-15 14:31:30 +0530

    #include<stdio.h> int main() {     int n,r,sum=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         n=n/10;
            if(r>sum)
            {
                sum=r;
            }
        }
        printf("%d",sum);
        return 0;
    }

---
+++ Largest_Digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        
+        if(r>sum)
+        {
+            sum=r;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
